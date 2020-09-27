ed194bae92e8172a01a71eb5481dd5778a0d0412
kimdera <39693234+kimdera@users.noreply.github.com>
2020-09-27 16:12:53 -0400

    Update string_generator.java

--- Assignment_1/string_generator.java
+++ Assignment_1/string_generator.java
@@ -1,13 +1,35 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
 
 public class string_generator {
-	
-//solving with recursion
+
+/* solving with recursion */
 	public static void string_generate(char []input_string, int index ) {
 		if (index == input_string.length) 
         { 
-            System.out.println(input_string); 
+            //System.out.println(input_string); 
+            
+			{
+			try {
+			      FileWriter myWriter = new FileWriter("out_1.txt",true);
+			                /* remove trailing spaces from partially initialized arrays */
+			      
+			      //myWriter.write(Arrays.toString(input_string).replace("[", "").replace("]", "").replace(",", "") + "\n");
+			      String string = new String(input_string);
+			      
+			      myWriter.write(string+ "\n");
+			      
+			      myWriter.close();
+			}
+			 catch (IOException e) {
+			      System.out.println("An error occurred.");
+			      e.printStackTrace();
+			    }
+
+			}
+            
             return; 
         } 
   
@@ -31,6 +53,19 @@ public class string_generator {
 	}
 
 	public static void main(String[] args) {
+		
+		try {
+		      File myObj = new File("out_1.txt");
+		      if (myObj.createNewFile()) {
+		        System.out.println("File created: " + myObj.getName());
+		      } else {
+		        System.out.println("File already exists.");
+		      }
+		    } catch (IOException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		
 		// TODO Auto-generated method stub
 		System.out.println("please enter a string: ");
 		Scanner sc= new Scanner(System.in);
