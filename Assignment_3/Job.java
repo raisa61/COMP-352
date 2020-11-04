126e1306057c9e112cebcd7041301e199ae80cd4
raisa61 <60297428+raisa61@users.noreply.github.com>
2020-11-03 21:19:38 -0500

    initial job class

--- Assignment_3/Job.java
+++ Assignment_3/Job.java
@@ -1,1 +1,172 @@
+public class Job {
+	private String jobName;
+	private int jobLength;
+	private int currentJobLength;
+	private int jobPriority;
+	private int finalPriority;
+	private long entryTime;
+	private long endTime;
+	private long waitTime;
+	
+	
+	/** parametrized constructor
+	 * @param jobName
+	 * @param jobLength
+	 * @param currentJobLength
+	 * @param jobPriority
+	 * @param finalPriority
+	 * @param entryTime
+	 * @param endTime
+	 * @param waitTime
+	 */
+	public Job(String jobName, int jobLength, int currentJobLength, int jobPriority, int finalPriority, long entryTime,
+			long endTime, long waitTime) {
+		super();
+		this.jobName = jobName;
+		this.jobLength = jobLength;
+		this.currentJobLength = currentJobLength;
+		this.jobPriority = jobPriority;
+		this.finalPriority = finalPriority;
+		this.entryTime = entryTime;
+		this.endTime = endTime;
+		this.waitTime = waitTime;
+	}
+
+
+	/**
+	 * @return the jobName
+	 */
+	public String getJobName() {
+		return jobName;
+	}
+
+
+	/**
+	 * @param jobName the jobName to set
+	 */
+	public void setJobName(String jobName) {
+		this.jobName = jobName;
+	}
+
+
+	/**
+	 * @return the jobLength
+	 */
+	public int getJobLength() {
+		return jobLength;
+	}
+
+
+	/**
+	 * @param jobLength the jobLength to set
+	 */
+	public void setJobLength(int jobLength) {
+		this.jobLength = jobLength;
+	}
+
+
+	/**
+	 * @return the currentJobLength
+	 */
+	public int getCurrentJobLength() {
+		return currentJobLength;
+	}
+
+
+	/**
+	 * @param currentJobLength the currentJobLength to set
+	 */
+	public void setCurrentJobLength(int currentJobLength) {
+		this.currentJobLength = currentJobLength;
+	}
+
+
+	/**
+	 * @return the jobPriority
+	 */
+	public int getJobPriority() {
+		return jobPriority;
+	}
+
+
+	/**
+	 * @param jobPriority the jobPriority to set
+	 */
+	public void setJobPriority(int jobPriority) {
+		this.jobPriority = jobPriority;
+	}
+
+
+	/**
+	 * @return the finalPriority
+	 */
+	public int getFinalPriority() {
+		return finalPriority;
+	}
+
+
+	/**
+	 * @param finalPriority the finalPriority to set
+	 */
+	public void setFinalPriority(int finalPriority) {
+		this.finalPriority = finalPriority;
+	}
+
+
+	/**
+	 * @return the entryTime
+	 */
+	public long getEntryTime() {
+		return entryTime;
+	}
+
+
+	/**
+	 * @param entryTime the entryTime to set
+	 */
+	public void setEntryTime(long entryTime) {
+		this.entryTime = entryTime;
+	}
+
+
+	/**
+	 * @return the endTime
+	 */
+	public long getEndTime() {
+		return endTime;
+	}
+
+
+	/**
+	 * @param endTime the endTime to set
+	 */
+	public void setEndTime(long endTime) {
+		this.endTime = endTime;
+	}
+
+
+	/**
+	 * @return the waitTime
+	 */
+	public long getWaitTime() {
+		return waitTime;
+	}
+
+
+	/**
+	 * @param waitTime the waitTime to set
+	 */
+	public void setWaitTime(long waitTime) {
+		this.waitTime = waitTime;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Now executing " + jobName + ". Job length: " + jobLength + " cycles. Current remaining length: " 
+	            + currentJobLength + ". Initial Priority: " + jobPriority + ". Current Priority: " + finalPriority ;
+	}
+	
+	
+}
 
