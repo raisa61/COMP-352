8341a53951baddd1469c06ffdddafcfd52d55208
Kim Dera <39693234+kimdera@users.noreply.github.com>
2020-11-11 15:52:27 -0500

    Update Vehicle.java

--- a-4/Vehicle.java
+++ a-4/Vehicle.java
@@ -1,14 +1,4 @@
 import java.util.Stack;
-//Assignment (3)
-//Programming Question 
-//Written by: (Marwan Jabbour, 40151859)
-//-----------------------------------------------------
-
-/**
-* 
-* @author marwanjabbour Name and ID (Marwan Jabbour, 40151859) Comp 352
-*         Assignment 3, Due Date Sunday, June 14, 2020
-*/
 
 public class Vehicle {
 
